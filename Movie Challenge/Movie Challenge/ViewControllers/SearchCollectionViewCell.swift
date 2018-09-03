c33507b6d9a90dfe5e387107968c782e94d39402
System Administrator <root@igors-Mac.local>
2018-09-03 11:18:46 -0700

    Criacao da tela de busca de filmescom as imagens

---
+++ Movie Challenge/Movie Challenge/ViewControllers/SearchCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SearchCollectionViewCell.swift
+//  Movie Challenge
+//
+//  Created by igor gomes arantes on 03/09/18.
+//  Copyright © 2018 igor gomes arantes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SearchCollectionViewCell : UICollectionViewCell{
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
---
+++ Movie Challenge/Movie Challenge/ViewControllers/SearchCollectionViewController.swift
@@ -0,0 +1,71 @@
+//
+//  SearchCollectionViewController.swift
+//  Movie Challenge
+//
+//  Created by igor gomes arantes on 03/09/18.
+//  Copyright © 2018 igor gomes arantes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SearchCollectionViewController : UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UISearchBarDelegate{
+
+    var moviePage = MoviePageDTO()
+    
+    @IBOutlet weak var movieCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return moviePage.results.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchCollectionCell", for: indexPath) as! SearchCollectionViewCell
+        
+        if let result: MovieDTO? = moviePage.results[indexPath.row]{
+            if let posterPath = result?.poster_path{
+                _ = MovieService.shared().getPosterFromAPI(path: posterPath, quality: Quality.low) { image in
+                    
+                    DispatchQueue.main.async() {
+                        cell.posterImageView.image = image
+                    }
+                }
+            }
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(moviePage.results[indexPath.row].title ?? "Nulo")
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
+        if(searchText == ""){
+            self.moviePage = MoviePageDTO()
+
+            DispatchQueue.main.async() {
+                self.movieCollectionView.reloadData()
+            }
+            
+        }else{
+            _ = MovieService.shared().findAllFromAPI(query: searchText){ newMoviePage in
+                self.moviePage = newMoviePage
+                
+                DispatchQueue.main.async() {
+                    self.movieCollectionView.reloadData()
+                }
+            }
+        }
+
+    }
+    
+}
